4a6793c079b8ab0ddf1b56f6e0588c4ad1db7108
Pawan Tilara <pawantilara123@gmail.com>
2021-10-25 21:20:57 +0530

    distace calaculation code added

--- src/main/java/com/location/simulator/service/LocationSimulatorService.java
+++ src/main/java/com/location/simulator/service/LocationSimulatorService.java
@@ -1,17 +1,22 @@
 package com.location.simulator.service;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import com.location.simulator.constants.ApiConstants;
 import com.location.simulator.model.GeoLocationResponse;
+import com.location.simulator.model.LatLngsModel;
 import com.location.simulator.model.LocationSimulatorRequestModel;
-import com.location.simulator.model.Route;
+import com.location.simulator.model.LocationSimulatorResponseModel;
 import com.location.simulator.utils.PropertiesUtil;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -23,23 +28,43 @@ public class LocationSimulatorService {
   @Autowired
   PropertiesUtil propertiesUtil;
 
-  public Object getLocationDetails(LocationSimulatorRequestModel locationSimulatorRequestModel){
-    String destination = locationSimulatorRequestModel.getDestination().getLats()+","+locationSimulatorRequestModel.getDestination().getLangs();
-    String origin = locationSimulatorRequestModel.getOrigin().getLats()+","+locationSimulatorRequestModel.getOrigin().getLangs();
+
+  public Object getLocationDetails(LocationSimulatorRequestModel locationSimulatorRequestModel) {
+    String destination = locationSimulatorRequestModel.getDestination().getLats() + "," + locationSimulatorRequestModel.getDestination().getLangs();
+    String origin = locationSimulatorRequestModel.getOrigin().getLats() + "," + locationSimulatorRequestModel.getOrigin().getLangs();
     String Key = ApiConstants.API_KEY;
     final HashMap<String, String> pathVariables = new HashMap<>();
     pathVariables.put(ApiConstants.DESTINATION, destination);
     pathVariables.put(ApiConstants.KEY, Key);
     pathVariables.put(ApiConstants.ORIGIN, origin);
     String url = propertiesUtil.getGeoLocationBaseApi().buildAndExpand(pathVariables).toUriString();
-    Object object  = new Object();
-    GeoLocationResponse geoLocationResponse = new GeoLocationResponse();
+    Object object = new Object();
+    LocationSimulatorResponseModel locationSimulatorResponseModel = new LocationSimulatorResponseModel();
     try {
-       object= restTemplate.getForObject(url, Object.class);
-       geoLocationResponse.setRoutes((Route[]) ((LinkedHashMap)object).get("routes"));
-    }catch (Exception e) {
+      object = restTemplate.getForObject(url, Object.class);
+      Gson gson = new Gson();
+      String json = gson.toJson(object);
+      JsonObject convertedObject = new Gson().fromJson(json, JsonObject.class);
+//      JsonElement routes= convertedObject.get("routes");//get(0).get("legs").get(0).get("distance").get("text");
+//      String x = "";
+//      List<Object> routeList = routes.get(0);
+//      JsonElement lat = position.get("lat");
+//      JsonElement lng = position.get("lng");
+//
+//      latLongList.add(lat.getAsDouble());
+//      latLongList.add(lng.getAsDouble());
+      double azimuth = propertiesUtil.calculateBearing(locationSimulatorRequestModel);
+      //System.out.println(azimuth);
+      ArrayList<LatLngsModel> cords = propertiesUtil.getLocations(50, azimuth, locationSimulatorRequestModel.getOrigin(), locationSimulatorRequestModel.getDestination());
+      locationSimulatorResponseModel.setLatLngsModelList(cords);
+
+    } catch (Exception e) {
       log.error(e.getMessage());
     }
     return object;
   }
 }
+
+
+
+
--- src/main/java/com/location/simulator/utils/PropertiesUtil.java
+++ src/main/java/com/location/simulator/utils/PropertiesUtil.java
@@ -1,11 +1,14 @@
 package com.location.simulator.utils;
 
-import com.location.simulator.constants.ApiConstants;
+import com.location.simulator.model.LatLngsModel;
+import com.location.simulator.model.LocationSimulatorRequestModel;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Component;
 import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
 
 @Configuration
 @Component
@@ -13,8 +16,75 @@ import org.springframework.web.util.UriComponentsBuilder;
 public class PropertiesUtil {
   @Value("${geo.location.api}")
   private String geoLocationApi;
-
+  private static final long RADIUS_OF_EARTH = 6371000;
   public UriComponentsBuilder getGeoLocationBaseApi() {
     return UriComponentsBuilder.fromHttpUrl(geoLocationApi);
+  }
+
+  public static double calculateBearing(LocationSimulatorRequestModel locationSimulatorRequestModel) {
+    double startLat = Math.toRadians(locationSimulatorRequestModel.getOrigin().getLats());
+    double startLong = Math.toRadians(locationSimulatorRequestModel.getOrigin().getLangs());
+    double endLat = Math.toRadians(locationSimulatorRequestModel.getDestination().getLats());
+    double endLong = Math.toRadians(locationSimulatorRequestModel.getDestination().getLangs());
+    double dLong = endLong - startLong;
+    double dPhi = Math
+            .log(Math.tan((endLat / 2.0) + (Math.PI / 4.0)) / Math.tan((startLat / 2.0) + (Math.PI / 4.0)));
+    if (Math.abs(dLong) > Math.PI) {
+      if (dLong > 0.0) {
+        dLong = -(2.0 * Math.PI - dLong);
+      } else {
+        dLong = (2.0 * Math.PI + dLong);
+      }
+    }
+    double bearing = (Math.toDegrees(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
+    return bearing;
+  }
+  public static LatLngsModel getDestinationLatLng(double lat, double lng, double azimuth, double distance) {
+    double radiusKm = RADIUS_OF_EARTH / 1000; // Radius of the Earth in km
+    double brng = Math.toRadians(azimuth); // Bearing is degrees converted to radians.
+    double d = distance / 1000; // Distance m converted to km
+    double lat1 = Math.toRadians(lat); // Current dd lat point converted to radians
+    double lon1 = Math.toRadians(lng); // Current dd long point converted to radians
+    double lat2 = Math.asin(
+            Math.sin(lat1) * Math.cos(d / radiusKm) + Math.cos(lat1) * Math.sin(d / radiusKm) * Math.cos(brng));
+    double lon2 = lon1 + Math.atan2(Math.sin(brng) * Math.sin(d / radiusKm) * Math.cos(lat1),
+            Math.cos(d / radiusKm) - Math.sin(lat1) * Math.sin(lat2));
+    // convert back to degrees
+    lat2 = Math.toDegrees(lat2);
+    lon2 = Math.toDegrees(lon2);
+    return new LatLngsModel(lat2, lon2);
+  }
+
+  public static ArrayList<LatLngsModel> getLocations(int interval, double azimuth, LatLngsModel start,
+                                                     LatLngsModel end) {
+
+
+    double d = getPathLength(start, end);
+    int totalDistance = (int) d;
+    int dist = (int) d / interval;
+    int coveredDist = interval;
+    ArrayList<LatLngsModel> coords = new ArrayList<>();
+    coords.add(new LatLngsModel(start.getLats(), start.getLangs()));
+    for (int distance = 0; distance < totalDistance; distance += interval) {
+      LatLngsModel coord = getDestinationLatLng(start.getLats(), start.getLangs(), azimuth, coveredDist);
+      coveredDist += interval;
+      coords.add(coord);
+    }
+    coords.add(new LatLngsModel(end.getLats(), end.getLangs()));
+
+    return coords;
+
+  }
+  public static double getPathLength(LatLngsModel start, LatLngsModel end) {
+    double lat1Rads = Math.toRadians(start.getLats());
+    double lat2Rads = Math.toRadians(end.getLats());
+    double deltaLat = Math.toRadians(end.getLats() - start.getLats());
+
+    double deltaLng = Math.toRadians(end.getLangs() - start.getLangs());
+    double a = Math.sin(deltaLat / 2) * Math.sin(deltaLat / 2)
+            + Math.cos(lat1Rads) * Math.cos(lat2Rads) * Math.sin(deltaLng / 2) * Math.sin(deltaLng / 2);
+    double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    double d = RADIUS_OF_EARTH * c;
+    return d;
   }
 }
